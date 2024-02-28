cd01181c1213f9612fd16c71b500fd83921f7ce9
Baldur van Lew <b.van_lew@lumc.nl>
2024-02-28 10:30:51 +0100

    Add a jupyter lab config to the package

--- src/MVJupyterPluginManager/__main__.py
+++ src/MVJupyterPluginManager/__main__.py
@@ -1,14 +1,29 @@
 import argparse
 import subprocess
+from pathlib import Path
 import os
 
 def main():
+    """
+    Start the jupyter lab with the package configuration file.
+    The user muat supply the the path tothe connection file
+    that contains the connection configuration written by the 
+    ManiVault Studio JupyterPlugin
+    """
     parser = argparse.ArgumentParser()
-    parser.add_argument("connectionfile", help="absolute path to connection file")
+    parser.add_argument("connectionfile", help="Required absolute path to connection file, must match the path in JupyterLauncher")
     args = parser.parse_args()
     os.environ["MANIVAULT_JUPYTERPLUGIN_CONNECTION_FILE"] = args.connectionfile
+    config_path = Path(Path(__file__).parent, "config", "jupyter_notebook_config.py")
     # python -m jupyterlab_server --ServerApp.kernel_manager_class=MVJupyterPluginManager.ExternalMappingKernelManager --KernelProvisionerFactory.default_provisioner_name=mvjupyterplugin-existing-provisioner
-    subprocess.run(["python", "-m", "jupyterlab_server", "--ServerApp.kernel_manager_class=MVJupyterPluginManager.ExternalMappingKernelManager", "--KernelProvisionerFactory.default_provisioner_name=mvjupyterplugin-existing-provisioner"])
+    print(f"Running jupyter lab with configuration at {config_path.absolute()}")
+    subprocess.run([
+        "python", 
+        "-m", 
+        "jupyter",
+        "lab", 
+        f"--config={config_path.absolute()}"
+    ])
 
 if __name__ == '__main__':
     main()
