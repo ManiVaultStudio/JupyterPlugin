6d05ba9451e9ae16527a33dc3828154fea960503
Alexander Vieth <vieth.alexander@gmx.net>
2024-12-05 13:25:28 +0100

    Fail gracefully

--- src/JupyterPlugin/XeusInterpreter.cpp
+++ src/JupyterPlugin/XeusInterpreter.cpp
@@ -3,6 +3,7 @@
 #include "MVData.h"
 
 #include <iostream>
+#include <stdexcept>
 #include <string>
 
 #include <xeus/xcomm.hpp>
@@ -11,8 +12,8 @@
 #include <xeus-python/xinterpreter.hpp>
 
 #include <pybind11/embed.h>
-#include <pybind11/pybind11.h>
 #include <pybind11/gil.h>
+#include <pybind11/pybind11.h>
 
 #include <QDebug>
 
@@ -39,9 +40,16 @@ void XeusInterpreter::configure_impl()
 
     py::gil_scoped_acquire acquire;
     py::module sys = py::module::import("sys");
-    py::module MVData_module = get_MVData_module();
-    MVData_module.doc() = "Provides access to low level ManiVaultStudio core functions";
-    sys.attr("modules")["mvstudio_core"] = MVData_module;
+
+    try {
+        py::module MVData_module = get_MVData_module();
+        MVData_module.doc() = "Provides access to low level ManiVaultStudio core functions";
+        sys.attr("modules")["mvstudio_core"] = MVData_module;
+    }
+    catch (const std::runtime_error& e)
+    {
+        std::cerr << "MVData modules failed to load: " << e.what() << std::endl;
+    }
 }
 
 // Execute incoming code and publish the result
