c1f1859e388118b56971f6eef48f168f452491c8
Alexander Vieth <vieth.alexander@gmx.net>
2025-10-02 13:16:32 +0200

    Update dependencies (#43)

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -28,7 +28,7 @@ endif()
 CPMAddPackage(
   NAME              nlohmann_json
   GITHUB_REPOSITORY	"nlohmann/json"
-  GIT_TAG           v3.12.0
+  GIT_TAG           ${nlohmann_json_VERSION}
   DOWNLOAD_ONLY     NO
   EXCLUDE_FROM_ALL  YES
   OPTIONS
@@ -114,15 +114,16 @@ include("${pybind_SOURCE_DIR}/tools/pybind11Common.cmake")
 CPMAddPackage(
     NAME                pybind11_json
     GITHUB_REPOSITORY   pybind/pybind11_json
-    GIT_TAG             ${pybind11_json_VERSION}
+    GIT_TAG             ${pybind11_json_VERSION_TAG}
+    VERSION             ${pybind11_json_VERSION}
     EXCLUDE_FROM_ALL    YES
 )
 
 CPMAddPackage(
     NAME                xeus-python
-    URL                 "https://github.com/jupyter-xeus/xeus-python/archive/refs/tags/${xeus-python_VERSION}.tar.gz"
+    GITHUB_REPOSITORY   jupyter-xeus/xeus-python
+    GIT_TAG             ${xeus-python_VERSION}
     EXCLUDE_FROM_ALL    YES
-    CPM_USE_LOCAL_PACKAGES ON
     OPTIONS 
         "XPYT_BUILD_TESTS OFF"
         "XPYT_BUILD_SHARED OFF"
@@ -144,8 +145,8 @@ endif()
 CPMAddPackage(
     NAME                md4qt
     GIT_REPOSITORY      https://invent.kde.org/libraries/md4qt.git
-    GIT_TAG             6c07e69824a269e0f6903caa4b59c905650a0749
-    VERSION             4.2.0           # see md4qt/CMakeLists.txt
+    GIT_TAG             a777b56c14679b94d48261ec465cf0e97650daad
+    VERSION             4.2.1
     DOWNLOAD_ONLY       YES
     EXCLUDE_FROM_ALL    YES
 )
